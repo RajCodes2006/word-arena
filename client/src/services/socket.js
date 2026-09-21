cccc2ba9f55c3f9424285bc641d128a08b99afa4
RAJ <ansh.rajj.1106@gmail.com>
2026-09-21 15:31:51 +0530

    Wire WordWars Socket.IO client

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -4,14 +4,15 @@ let socket;
 
 export function connectSocket(url) {
   if (!socket) {
-    socket = io(url, { autoConnect: true });
+    socket = io(url, {
+      autoConnect: true,
+      transports: ['websocket', 'polling']
+    });
   }
   return socket;
 }
 
 export function getSocket() {
-  if (!socket) {
-    throw new Error('Socket is not connected yet.');
-  }
+  if (!socket) throw new Error('Socket is not connected yet.');
   return socket;
 }
