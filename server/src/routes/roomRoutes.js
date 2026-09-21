f6156ababa920f9150ff2f7e84aeb805a890596e
RAJ <ansh.rajj.1106@gmail.com>
2026-09-21 15:31:23 +0530

    Add WordWars room routes

--- server/src/routes/roomRoutes.js
+++ server/src/routes/roomRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createRoom, getRoom } from '../controllers/roomController.js';
+import { createRoom, createPlayerId, getRoom } from '../controllers/roomController.js';
 
 const router = Router();
 
 router.post('/', createRoom);
+router.post('/player-id', createPlayerId);
 router.get('/:roomId', getRoom);
 
 export default router;
