1786eeb147463a49111ddf989544c2d60547e3d7
RAJ <ansh.rajj.1106@gmail.com>
2026-09-23 01:04:07 +0530

    Route unknown paths to 404

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import NotFound from './NotFound';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {window.location.pathname === '/' ? <App /> : <NotFound />}
   </React.StrictMode>
 );
