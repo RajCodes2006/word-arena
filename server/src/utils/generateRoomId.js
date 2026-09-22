4a205e97339a4022c3ed5556fb38c30d93a8d617
RAJ <ansh.rajj.1106@gmail.com>
2026-09-23 01:27:32 +0530

    Increase room code entropy

--- server/src/utils/generateRoomId.js
+++ server/src/utils/generateRoomId.js
@@ -2,7 +2,7 @@ const CHARS = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
 
 export function generateRoomId() {
   let id = 'WW-';
-  for (let i = 0; i < 4; i += 1) {
+  for (let i = 0; i < 6; i += 1) {
     id += CHARS[Math.floor(Math.random() * CHARS.length)];
   }
   return id;
